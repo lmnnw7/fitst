09e6360ab0836f4ca8ff5a41b5429ccede93f07e
lmnnw7 <13125152832qq.com>
2023-10-22 23:36:21 +0800

    3

--- src/main/java/com/lh/FJYLApplication.java
+++ src/main/java/com/lh/FJYLApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeseApplication {
+public class FJYLApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SeseApplication.class, args);
+        SpringApplication.run(FJYLApplication.class, args);
     }
 
 }
