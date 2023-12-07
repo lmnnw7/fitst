3f8c0b904be9f0ae65dab5249c4c760e9d06e5c7
lmnnw7 <13125152832qq.com>
2023-12-07 14:58:31 +0800

    修正一些错误

--- src/main/java/com/lh/controller/PostController.java
+++ src/main/java/com/lh/controller/PostController.java
@@ -1,10 +1,67 @@
 package com.lh.controller;
 
+import com.lh.pojo.Post;
+import com.lh.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/api")
 @RestController
 public class PostController {
-    
+
+    @Autowired(required = false)
+    private PostService postService;
+
+    //查找所有贴子
+    @RequestMapping("/findAllPost")
+    public ResponseEntity<?> findAllPost(){
+        List<Post> postList=postService.findAllPost();
+        return ResponseEntity.ok(postList);
+    }
+
+    //根据id删除贴子
+    @RequestMapping("/deletePostByID")
+    public ResponseEntity<?> deletePostByID(@RequestParam Integer id){
+        int i=postService.deletePostByID(id);
+        if(i>0){
+            return ResponseEntity.ok(i);
+        }
+        else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    //更新贴子
+    @RequestMapping("/updatePost")
+    public ResponseEntity<?> updatePost(@RequestBody Post Post){
+        try{
+            int i=postService.updatePost(Post);
+            return ResponseEntity.ok(i);
+        }catch (Exception exception){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    //查询贴子，模糊查询
+    @RequestMapping("/findPostByStr")
+    public ResponseEntity<?> findPostByStr(@RequestParam(value="searchStr") String searchStr){
+        List<Post> postList=postService.findPostByStr(searchStr);
+        return ResponseEntity.ok(postList);
+    }
+
+    //添加贴子
+    @RequestMapping("addPost")
+    public ResponseEntity<?> addPost(@RequestBody Post Post){
+        try{
+            int i=postService.insertPost(Post);
+            return ResponseEntity.ok(i);
+        }catch (Exception exception){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
 }
